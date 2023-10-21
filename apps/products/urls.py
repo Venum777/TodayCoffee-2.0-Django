fa87000ca28d752a2defa2ffd129336f69f3a788
Venum777 <venums46@gmail.com>
2023-10-21 23:33:12 +0600

    fast commit

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -5,9 +5,8 @@ from django.urls import path
 from .views import AllProductView, ProductView
 from . import views
 
-
 urlpatterns = [
     path('all/', AllProductView.as_view()),
-    path('', ProductView.as_view()),
+    path('', ProductView.as_view(), name='home'),
     path('genre/<int:genre_id>/', ProductView.as_view()),
 ]
